cbee663c7e902d6f6bd361b8f66db6ed23a3c5a6
Fırat Alçın <firatalcin04@gmail.com>
2022-05-19 14:59:54 +0300

    Update

---
+++ Java101/Donguler/Odevler/_1-100_Asal_Sayilar/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int count = 0;
+        for(int i = 2; i<100; i++){
+            for(int j = 1; j<=i; j++){
+                if(i % j == 0){
+                    count++;
+                }
+            }
+            if(count == 2){
+                System.out.print(i + ", ");
+            }
+            count = 0;
+        }
+    }
+}
